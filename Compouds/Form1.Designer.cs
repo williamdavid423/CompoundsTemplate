f40132870ded8ec9c82775a54e9a495d798d4052
Tony Theodoropoulos <theodot@outlook.com>
2021-02-17 14:43:40 -0500

    Add project files.

---
+++ Compouds/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace Compouds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.heartButton = new System.Windows.Forms.Button();
+            this.diamondButton = new System.Windows.Forms.Button();
+            this.clubButton = new System.Windows.Forms.Button();
+            this.spadeButton = new System.Windows.Forms.Button();
+            this.orButton = new System.Windows.Forms.Button();
+            this.suitOutput = new System.Windows.Forms.Label();
+            this.thisButton = new System.Windows.Forms.Button();
+            this.thatButton = new System.Windows.Forms.Button();
+            this.andButton = new System.Windows.Forms.Button();
+            this.andLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.hurricaneButton = new System.Windows.Forms.Button();
+            this.categoryOutput = new System.Windows.Forms.Label();
+            this.categoryInput = new System.Windows.Forms.TextBox();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // heartButton
+            // 
+            this.heartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.heartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heartButton.ForeColor = System.Drawing.Color.White;
+            this.heartButton.Location = new System.Drawing.Point(16, 38);
+            this.heartButton.Name = "heartButton";
+            this.heartButton.Size = new System.Drawing.Size(110, 39);
+            this.heartButton.TabIndex = 0;
+            this.heartButton.Text = "Hearts";
+            this.heartButton.UseVisualStyleBackColor = true;
+            this.heartButton.Click += new System.EventHandler(this.heartButton_Click);
+            // 
+            // diamondButton
+            // 
+            this.diamondButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.diamondButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diamondButton.ForeColor = System.Drawing.Color.White;
+            this.diamondButton.Location = new System.Drawing.Point(138, 38);
+            this.diamondButton.Name = "diamondButton";
+            this.diamondButton.Size = new System.Drawing.Size(110, 39);
+            this.diamondButton.TabIndex = 1;
+            this.diamondButton.Text = "Diamonds";
+            this.diamondButton.UseVisualStyleBackColor = true;
+            this.diamondButton.Click += new System.EventHandler(this.diamondButton_Click);
+            // 
+            // clubButton
+            // 
+            this.clubButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clubButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clubButton.ForeColor = System.Drawing.Color.White;
+            this.clubButton.Location = new System.Drawing.Point(16, 90);
+            this.clubButton.Name = "clubButton";
+            this.clubButton.Size = new System.Drawing.Size(110, 39);
+            this.clubButton.TabIndex = 2;
+            this.clubButton.Text = "Clubs";
+            this.clubButton.UseVisualStyleBackColor = true;
+            this.clubButton.Click += new System.EventHandler(this.clubButton_Click);
+            // 
+            // spadeButton
+            // 
+            this.spadeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.spadeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spadeButton.ForeColor = System.Drawing.Color.White;
+            this.spadeButton.Location = new System.Drawing.Point(138, 90);
+            this.spadeButton.Name = "spadeButton";
+            this.spadeButton.Size = new System.Drawing.Size(110, 39);
+            this.spadeButton.TabIndex = 3;
+            this.spadeButton.Text = "Spades";
+            this.spadeButton.UseVisualStyleBackColor = true;
+            this.spadeButton.Click += new System.EventHandler(this.spadeButton_Click);
+            // 
+            // orButton
+            // 
+            this.orButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.orButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orButton.ForeColor = System.Drawing.Color.White;
+            this.orButton.Location = new System.Drawing.Point(76, 146);
+            this.orButton.Name = "orButton";
+            this.orButton.Size = new System.Drawing.Size(110, 39);
+            this.orButton.TabIndex = 4;
+            this.orButton.Text = "Or Check";
+            this.orButton.UseVisualStyleBackColor = true;
+            this.orButton.Click += new System.EventHandler(this.orButton_Click);
+            // 
+            // suitOutput
+            // 
+            this.suitOutput.ForeColor = System.Drawing.Color.White;
+            this.suitOutput.Location = new System.Drawing.Point(12, 188);
+            this.suitOutput.Name = "suitOutput";
+            this.suitOutput.Size = new System.Drawing.Size(236, 57);
+            this.suitOutput.TabIndex = 5;
+            this.suitOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // thisButton
+            // 
+            this.thisButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.thisButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thisButton.ForeColor = System.Drawing.Color.White;
+            this.thisButton.Location = new System.Drawing.Point(17, 38);
+            this.thisButton.Name = "thisButton";
+            this.thisButton.Size = new System.Drawing.Size(110, 39);
+            this.thisButton.TabIndex = 6;
+            this.thisButton.Text = "This";
+            this.thisButton.UseVisualStyleBackColor = true;
+            this.thisButton.Click += new System.EventHandler(this.thisButton_Click);
+            // 
+            // thatButton
+            // 
+            this.thatButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.thatButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thatButton.ForeColor = System.Drawing.Color.White;
+            this.thatButton.Location = new System.Drawing.Point(139, 38);
+            this.thatButton.Name = "thatButton";
+            this.thatButton.Size = new System.Drawing.Size(110, 39);
+            this.thatButton.TabIndex = 7;
+            this.thatButton.Text = "That";
+            this.thatButton.UseVisualStyleBackColor = true;
+            this.thatButton.Click += new System.EventHandler(this.thatButton_Click);
+            // 
+            // andButton
+            // 
+            this.andButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.andButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.andButton.ForeColor = System.Drawing.Color.White;
+            this.andButton.Location = new System.Drawing.Point(79, 146);
+            this.andButton.Name = "andButton";
+            this.andButton.Size = new System.Drawing.Size(110, 39);
+            this.andButton.TabIndex = 8;
+            this.andButton.Text = "And Check";
+            this.andButton.UseVisualStyleBackColor = true;
+            this.andButton.Click += new System.EventHandler(this.andButton_Click);
+            // 
+            // andLabel
+            // 
+            this.andLabel.ForeColor = System.Drawing.Color.White;
+            this.andLabel.Location = new System.Drawing.Point(13, 188);
+            this.andLabel.Name = "andLabel";
+            this.andLabel.Size = new System.Drawing.Size(236, 57);
+            this.andLabel.TabIndex = 9;
+            this.andLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-5, -1);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(901, 52);
+            this.titleLabel.TabIndex = 30;
+            this.titleLabel.Text = "Switch Blocks and Compound if Statements";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.thatButton);
+            this.groupBox1.Controls.Add(this.thisButton);
+            this.groupBox1.Controls.Add(this.andLabel);
+            this.groupBox1.Controls.Add(this.andButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(598, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 253);
+            this.groupBox1.TabIndex = 31;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "AND condition";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.orButton);
+            this.groupBox2.Controls.Add(this.heartButton);
+            this.groupBox2.Controls.Add(this.diamondButton);
+            this.groupBox2.Controls.Add(this.suitOutput);
+            this.groupBox2.Controls.Add(this.clubButton);
+            this.groupBox2.Controls.Add(this.spadeButton);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(315, 72);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(262, 253);
+            this.groupBox2.TabIndex = 32;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "OR condition";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.hurricaneButton);
+            this.groupBox3.Controls.Add(this.categoryOutput);
+            this.groupBox3.Controls.Add(this.categoryInput);
+            this.groupBox3.Controls.Add(this.categoryLabel);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.Color.White;
+            this.groupBox3.Location = new System.Drawing.Point(30, 72);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(262, 253);
+            this.groupBox3.TabIndex = 33;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Swtich Block";
+            // 
+            // hurricaneButton
+            // 
+            this.hurricaneButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hurricaneButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hurricaneButton.ForeColor = System.Drawing.Color.White;
+            this.hurricaneButton.Location = new System.Drawing.Point(124, 93);
+            this.hurricaneButton.Name = "hurricaneButton";
+            this.hurricaneButton.Size = new System.Drawing.Size(110, 39);
+            this.hurricaneButton.TabIndex = 3;
+            this.hurricaneButton.Text = "Hurricane";
+            this.hurricaneButton.UseVisualStyleBackColor = true;
+            this.hurricaneButton.Click += new System.EventHandler(this.hurricaneButton_Click);
+            // 
+            // categoryOutput
+            // 
+            this.categoryOutput.Location = new System.Drawing.Point(28, 188);
+            this.categoryOutput.Name = "categoryOutput";
+            this.categoryOutput.Size = new System.Drawing.Size(206, 57);
+            this.categoryOutput.TabIndex = 2;
+            this.categoryOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // categoryInput
+            // 
+            this.categoryInput.Location = new System.Drawing.Point(182, 45);
+            this.categoryInput.Name = "categoryInput";
+            this.categoryInput.Size = new System.Drawing.Size(52, 24);
+            this.categoryInput.TabIndex = 1;
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Location = new System.Drawing.Point(25, 48);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(140, 18);
+            this.categoryLabel.TabIndex = 0;
+            this.categoryLabel.Text = "Enter a Category:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(894, 358);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Conditions";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button heartButton;
+        private System.Windows.Forms.Button diamondButton;
+        private System.Windows.Forms.Button clubButton;
+        private System.Windows.Forms.Button spadeButton;
+        private System.Windows.Forms.Button orButton;
+        private System.Windows.Forms.Label suitOutput;
+        private System.Windows.Forms.Button thisButton;
+        private System.Windows.Forms.Button thatButton;
+        private System.Windows.Forms.Button andButton;
+        private System.Windows.Forms.Label andLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label categoryOutput;
+        private System.Windows.Forms.TextBox categoryInput;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.Button hurricaneButton;
+    }
+}
+
---
+++ Compouds/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compouds
+{
+    public partial class Form1 : Form
+    {
+        string suit = "";
+        bool thisCondition = false;
+        bool thatCondition = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hurricaneButton_Click(object sender, EventArgs e)
+        {
+            //get the inputted category
+
+            //check the inputted category with a switch block
+        }
+
+        private void heartButton_Click(object sender, EventArgs e)
+        {
+            //set suit value
+
+            //set button colour to goldenrod, (other buttons black)
+        }
+
+        private void diamondButton_Click(object sender, EventArgs e)
+        {
+            //set suit value
+
+            //set button colour to goldenrod, (other buttons black)
+        }
+
+        private void clubButton_Click(object sender, EventArgs e)
+        {
+            //set suit value
+
+            //set button colour to goldenrod, (other buttons black)
+        }
+
+        private void spadeButton_Click(object sender, EventArgs e)
+        {
+            //set suit value
+
+            //set button colour to goldenrod, (other buttons black)
+        }
+
+        private void orButton_Click(object sender, EventArgs e)
+        {
+            //use compound if statements to check what suit
+            //has been selected and output the colour of the suit
+        }
+
+        private void thisButton_Click(object sender, EventArgs e)
+        {
+            //set thisCondition to opposite of what it currently 
+            //is, and change BackColor to Black for false and 
+            //Goldenrod for true
+        }
+
+        private void thatButton_Click(object sender, EventArgs e)
+        {
+            //set thatCondition to opposite of what it currently 
+            //is, and change BackColor to Black for false and 
+            //Goldenrod for true
+        }
+
+        private void andButton_Click(object sender, EventArgs e)
+        {
+            //use compuond if statements to see if both conditions
+            //are true, false, or different
+        }
+    }
+}
